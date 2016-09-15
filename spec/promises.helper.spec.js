79e1e15e08f27c847550eecd178cbfb87e8ee0c1
Lucas Nagle <lnagle@gmail.com>
2016-09-15 07:50:50 -0500

    Add testing for promises helper, in particular how it handles different versions of jasmine

--- spec/promises.helper.spec.js
+++ spec/promises.helper.spec.js
@@ -1,8 +1,44 @@
+/* eslint-disable no-underscore-dangle */
 "use strict";
 
 describe("promises.helper", () => {
-    var check;
+    var check, mockRequire;
 
+    mockRequire = require("mock-require");
+
+    /**
+     * Provides a way of easily passing in a mock Jasmine environment.
+     *
+     * @param {string} filePath
+     * @param {number} versionNumber
+     * @return {Object}
+     */
+    function versionTestHelper(filePath, versionNumber) {
+        var mock, oldJasmine;
+
+        /**
+         * Resets Jasmine in the global namespace to the actual Jasmine, rather
+         * than a mock. Without this, the other spec files will fail.
+         */
+        function cleanup() {
+            global.jasmine = oldJasmine;
+        }
+
+        oldJasmine = global.jasmine;
+        mock = mockRequire.reRequire(`./mock/jasmine${versionNumber}-mock.js`)();
+
+        try {
+            global.jasmine = mock;
+            mockRequire.reRequire(filePath);
+        } catch (err) {
+            cleanup();
+            throw err;
+        }
+
+        cleanup();
+
+        return mock;
+    }
     describe("check for promises working", () => {
         beforeEach(() => {
             check = 0;
@@ -28,20 +64,34 @@ describe("promises.helper", () => {
             expect(check).toBe(2);
         });
     });
-    it("has patched the methods", () => {
-        expect(jasmine.getEnv().patchedForPromises).toBe(true);
-        expect(jasmine.getEnv().afterEach.patchedForPromises).toBe(true);
-        expect(jasmine.getEnv().beforeEach.patchedForPromises).toBe(true);
-        expect(jasmine.getEnv().fit.patchedForPromises).toBe(true);
-        expect(jasmine.getEnv().it.patchedForPromises).toBe(true);
-        expect(jasmine.getEnv().xit.patchedForPromises).toBe(true);
-    });
-    beforeEach(() => {
-        delete require.cache[require.resolve("../lib/promises.helper.js")];
-        jasmine.Env.prototype.someKey = true;
-        require("../lib/promises.helper");
-    });
-    it("handles different Jasmine versions", () => {
-        expect(jasmine.Env.prototype.iit.patchedForPromises).toBe(true);
+    describe("Version compatability:", () => {
+        var jasmineMock;
+
+        describe("Jasmine versions less than 2", () => {
+            beforeEach(() => {
+                jasmineMock = versionTestHelper("../lib/promises.helper.js", 1).currentEnv_;
+            });
+            it("has patched the methods", () => {
+                expect(jasmineMock.patchedForPromises).toBe(true);
+                expect(jasmineMock.afterEach.patchedForPromises).toBe(true);
+                expect(jasmineMock.beforeEach.patchedForPromises).toBe(true);
+                expect(jasmineMock.iit.patchedForPromises).toBe(true);
+                expect(jasmineMock.it.patchedForPromises).toBe(true);
+                expect(jasmineMock.xit.patchedForPromises).toBe(true);
+            });
+        });
+        describe("Jasmine versions 2 and up", () => {
+            beforeEach(() => {
+                jasmineMock = versionTestHelper("../lib/promises.helper.js", 2).currentEnv_;
+            });
+            it("has patched the methods", () => {
+                expect(jasmineMock.patchedForPromises).toBe(true);
+                expect(jasmineMock.afterEach.patchedForPromises).toBe(true);
+                expect(jasmineMock.beforeEach.patchedForPromises).toBe(true);
+                expect(jasmineMock.fit.patchedForPromises).toBe(true);
+                expect(jasmineMock.it.patchedForPromises).toBe(true);
+                expect(jasmineMock.xit.patchedForPromises).toBe(true);
+            });
+        });
     });
 });
