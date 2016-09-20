9045af8dbb64493a8edc241d1ba96706103883c4
Lucas Nagle <lnagle@gmail.com>
2016-09-20 10:18:41 -0500

    Add documentation

--- spec/mock/jasmine-nonprototypal-mock.js
+++ spec/mock/jasmine-nonprototypal-mock.js
@@ -3,7 +3,15 @@
 module.exports = () => {
     var mock;
 
+    /**
+     * An alternative mock of Jasmine for version that did away with the
+     * prototypal methods
+     */
     class JasmineEnv {
+        /**
+         * Mocks methods that need to be wrapped and patched by the promises
+         * helper.
+         */
         constructor() {
             this.afterEach = () => {};
             this.beforeEach = () => {};
